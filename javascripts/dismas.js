215218e9f1b508ed694e667084d1c0993189f123
Daniel Llinas <danllinas@gmail.com>
2015-12-30 09:58:49 -0500

    Trying to change JS for mobile.

--- javascripts/dismas.js
+++ javascripts/dismas.js
@@ -6,14 +6,17 @@ ready = function() {
 scroll = function (){
   var height = $("nav").height();
 
-  if ($(window).scrollTop() >= height){
-    $("nav").addClass("navbar-fixed-top").height( 85 );
-    $(".navbar-brand img").addClass("logo");
-    console.log("WORKING");
-  } else {
-    $("nav").removeClass("navbar-fixed-top").height( 235 );
-    $(".navbar-brand img").removeClass("logo");
+  if (screen.width > 600) {
+    if ($(window).scrollTop() >= height){
+      $("nav").addClass("navbar-fixed-top").height( 85 );
+      $(".navbar-brand img").addClass("logo");
+      console.log("WORKING");
+    } else {
+      $("nav").removeClass("navbar-fixed-top").height( 235 );
+      $(".navbar-brand img").removeClass("logo");
+    }
   }
+  
 };
 
 $(document).scroll(scroll);
