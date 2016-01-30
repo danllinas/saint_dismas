7161d079d5681ad3be86c24a21ef4d57b0595d1f
Daniel Llinas <danllinas@gmail.com>
2016-01-30 12:04:29 -0500

    fixed js issue.

--- javascripts/dismas.js
+++ javascripts/dismas.js
@@ -1,21 +1,19 @@
 $(window).load(function(){
   height = $(".navbar-wrapper").height();
+  $(document).scroll(scroll);
+  $(window).resize(scroll);
 });
 
 // height = 0;
 
 scroll = function (){
 
-    if ($(window).scrollTop() >= height){
-      $("nav").addClass("navbar-fixed-top");
-      $(".navbar-brand img").addClass("logo");
-    } else {
-      $("nav").removeClass("navbar-fixed-top");
-      $(".navbar-brand img").removeClass("logo");
-    }
+  if ($(window).scrollTop() >= height){
+    $("nav").addClass("navbar-fixed-top");
+    $(".navbar-brand img").addClass("logo");
+  } else {
+    $("nav").removeClass("navbar-fixed-top");
+    $(".navbar-brand img").removeClass("logo");
+  }
 
 };
-
-$(document).scroll(scroll);
-
-$(window).resize(scroll);
