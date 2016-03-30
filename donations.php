881b067fc424eb2ba1bec54e34209bb27366926f
Adnan Mateen <adnanmateen@outlook.com>
2016-03-30 20:03:12 +0500

    chaning to php files

--- donations.php
+++ donations.php
@@ -1,6 +1,11 @@
+<?php
+  if(!empty($_POST)){
+    var_dump($_POST);
+    exit;
+  }
+ ?>
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,14 +18,13 @@
   <link href='https://fonts.googleapis.com/css?family=Lato:400,400italic' rel='stylesheet' type='text/css'>
   <title>Saint Dismas - How to Help</title>
 </head>
-
 <body>
   <div class="navbar-wrapper" id="nav-partial">
     <nav class="navbar navbar-default navbar-custom">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header text-center">
-          <a class="navbar-brand" href="index.html#"><img src="images/logo_small.png" class="img-responsive" alt="Saint Dismas Prison Ministry Logo"></a>
+          <a class="navbar-brand" href="#"><img src="images/logo_small.png" class="img-responsive" alt="Saint Dismas Prison Ministry Logo"></a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
@@ -57,7 +61,7 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">How To Help <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="donations.html">Donations</a></li>
+                <li><a href="donations.php">Donations</a></li>
               </ul>
             </li>
             <li class="dropdown">
@@ -108,10 +112,11 @@
         <p class="text-center">
           <button class="btn btn-lg" id="trigger" style="color:white;background-color:#5a0c12;font-family:'Fredericka the Great',cursive;">Donate Online</button>
         </p><br>
-        <div class="container-fluid">
+        <div class="container-fluid text-left">
           <div class="row">
             <div class="col-xs-8 col-xs-offset-2 text-center" id="donate">
-              <form action="MAILTO:danllinas@gmail.com" method="post" class="form-horizontal">
+              <form action="donations.php" method="post" class="form-horizontal">
+                <span class="payment-errors"></span>
                 <div class="form-group">
                   <label for="firstName" class="col-xs-3">First Name</label>
                   <div class="col-xs-9">
@@ -160,6 +165,26 @@
                     <input type="text" name="zip" class="form-control">
                   </div>
                 </div>
+                <div class="form-group">
+                  <label for="zip" class="col-xs-3">Card Number</label>
+                  <div class="col-xs-9">
+                    <input type="text" size="20" data-stripe="number" class="form-control"/>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="zip" class="col-xs-3">CVC</label>
+                  <div class="col-xs-9">
+                    <input type="text" size="4" data-stripe="cvc" class="form-control"/>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="zip" class="col-xs-3">Expiration (MM/YYYY)</label>
+                  <div class="col-xs-9">
+                    <input type="text" size="2" data-stripe="exp-month" class="form-control inline-inputs"/>
+                    <span> / </span>
+                    <input type="text" size="4" data-stripe="exp-year" class="form-control inline-inputs"/>
+                  </div>
+                </div>
                 <div class="col-xs-9 col-xs-offset-3 text-center">
                   <input type="submit" class="btn btn-lg" style="color:white;background-color:#5a0c12;font-family:'Fredericka the Great',cursive;">
                 </div>
@@ -193,7 +218,7 @@
           </p>
         </div>
         <div class="col-xs-12 col-sm-4 col-md-4 text-center">
-          <a href="index.html"><img src="images/logo_small.png" class="logo img-responsive" alt="Saint Dismas Prison Ministry Logo" /></a>
+          <a href=""><img src="images/logo_small.png" class="logo img-responsive" alt="Saint Dismas Prison Ministry Logo" /></a>
         </div>
         <div class="col-xs-12 col-sm-4 col-md-4 text-center">
           <p>
